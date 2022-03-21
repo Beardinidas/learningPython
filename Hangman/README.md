9287b1f9d2fa402779fea337bceb32bf10612bb2
Beardinidas <33542863+Beardinidas@users.noreply.github.com>
2022-03-21 15:56:30 +0000

    Added Framework for Hangman

---
+++ Hangman/README.md
@@ -0,0 +1,4 @@
+# Hangman
+
+Imports a list of words which a random word will be picked from. 
+Guess the word before you loose all your lives
---
+++ Hangman/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import random
+import os
+
+
+class Style:
+	RESET = '\033[0m'
+	RED = '\033[31m'
+	GREEN = '\033[32m'
+	YELLOW = '\033[33m'
+	BLUE = '\033[34m'
+
+
+def main():
+	#TODO
+
+
+def clearConsole():
+        os.system('cls' if os.name=='nt' else 'clear')
+
+
+if __name__ == '__main__':
+        main()

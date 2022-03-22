1df982a04198a9700ee0739a3db82818aced1392
Beardinidas <33542863+Beardinidas@users.noreply.github.com>
2022-03-22 19:20:20 +0000

    User Input Implemented

--- Hangman/README.md
+++ Hangman/README.md
@@ -2,3 +2,8 @@
 
 Imports a list of words which a random word will be picked from. 
 Guess the word before you loose all your lives
+
+
+## Note
+Please run the following command to install required packages
+pip install -r requirements
--- Hangman/main.py
+++ Hangman/main.py
@@ -2,6 +2,8 @@
 
 import random
 import os
+import base64
+from PyDictionary import PyDictionary
 
 
 class Style:
@@ -12,13 +14,109 @@ class Style:
 	BLUE = '\033[34m'
 
 
+HANGMAN = ['''
+ +---+
+     |
+     |
+     |
+    ===''', '''
+ +---+
+ O   |
+     |
+     |
+    ===''', '''
+ +---+
+ O   |
+ |   |
+     |
+    ===''', '''
+ +---+
+ O   |
+/|   |
+     |
+    ===''', '''
+ +---+
+ O   |
+/|\  |
+     |
+    ===''', '''
+ +---+
+ O   |
+/|\  |
+/    |
+    ===''', '''
+ +---+
+ O   |
+/|\  |
+/ \  |
+    ===
+''']
+
+
+dict = PyDictionary()
+wordlist = list()
+correctGuesses = []
+incorrectGuesses = []
+currentGuessState = ''
+
 def main():
-	#TODO
+	#Import List of Words
+	loadListOfWords()
+
+	#Get a Random Word from List
+	secret = base64.b64decode(random.choice(wordlist)).decode('utf-8')
+
+	#Start Game
+	playGame(secret)
+
+	#Get Definition of Word + Print
+	definition = dict.meaning(secret)
+	print(f'{secret} : {definition}')
+
+
+
+def loadListOfWords():
+	#Import List of Words
+	with open('wordlist') as f:
+		#Import Each Line 'Word' at a Time
+		for line in f:
+			wordlist.append(line.replace('\n', '') + '==')
+
+
+def playGame(secret):
+	#Split Secret Word into Characters
+	charsInSecret = list(secret)
+
+	life = 0
+	while life < len(HANGMAN):
+		print(Style.YELLOW + 'HANGMAN\n' + Style.RESET)
+		#Get Input From Uses
+		guess = input("Please Enter a Guess: ")
+
+		if guess in correctGuesses:
+			input('You Have Already Guessed {guess} which was ' + Style.GREEN + 'CORRECT!' + Style.RESET)
+		elif guess in incorrectGuesses:
+			input('You Have Already {guess} which was ' + Style.RED + 'INCORRECT!' + Style.RESET)
+		else:
+			if guess in charsInSecret:
+				#IF CHARACTER IN SECRET
+				correctGuesses.append(guess)
+				input('You Have Guessed a Correct Letter!')
+				getGuessState(guess)
+			else:
+				#IF CHARACTER NOT IN SECRET
+				incorrectGuesses.append(guess)
+				print(Style.RED + HANGMAN[life] + Style.RESET)
+				input(f'Guess {guess} Is Not Correct')
+				life += 1
+
+
+def getGuessState(guess):
+	#TODO Guess State displaying Current Corect Guesses in positions
 
 
 def clearConsole():
-        os.system('cls' if os.name=='nt' else 'clear')
-
+	os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == '__main__':
         main()

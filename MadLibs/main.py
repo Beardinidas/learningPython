aececb558ec5e5922363b67f4b67ca61d9dc95ca
Beardinidas <33542863+Beardinidas@users.noreply.github.com>
2022-03-23 01:09:23 +0000

    Added Folder Structure for MadLibs

---
+++ MadLibs/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import random
+import os
+
+
+class Style:
+	RESET = '\033[0m'
+	RED = '\033[31m'
+	GREEN = '\033[32m'
+	YELLOW = '\033[33m'
+	BLUE = '\033[34m'
+	underLine = '\033[4m'
+
+
+def main():
+	#TODO
+	print('TODO')
+
+
+def clearConsole():
+        os.system('cls' if os.name=='nt' else 'clear')
+
+
+if __name__ == '__main__':
+        main()

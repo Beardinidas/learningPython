5b94046f48dc2f9100e1e32969c0061abdd18e4c
Beardinidas <33542863+Beardinidas@users.noreply.github.com>
2022-03-26 00:00:32 +0000

    CountDown Timer Console Completed

--- CountdownTimer/main.py
+++ CountdownTimer/main.py
@@ -2,7 +2,7 @@
 
 import random
 import os
-import datetime
+import time
 
 class Style:
 	RESET = '\033[0m'
@@ -14,7 +14,32 @@ class Style:
 
 
 def main():
-	print('TODO')
+	hours = input('Enter Hours: ')
+	mins = input('Enter Minutes: ')
+	seconds = input('Enter Seconds: ')
+
+	#Convert Total Time into Seconds
+	hoursInSeconds = int(hours) * 3600
+	minsInSeconds = int(mins) * 60
+	totalTime = int(hoursInSeconds) + int(minsInSeconds) + int(seconds)
+
+	clearConsole()
+	input(f'{Style.GREEN}Hit Enter To Start Timer{Style.RESET}')
+
+	countDown(totalTime)
+
+	print(f'{Style.RED}TIMES UP!{Style.RESET}')
+
+def countDown(totalTime):
+	while totalTime > 0:
+		if totalTime > 10:
+			print(f'{Style.GREEN}{str(totalTime)}{Style.RESET}')
+			totalTime -= 1
+			time.sleep(1)
+		if totalTime <=10 and totalTime > 0:
+			print(f'{Style.RED}{str(totalTime)}{Style.RESET}')
+			totalTime -= 1
+			time.sleep(1)
 
 def clearConsole():
         os.system('cls' if os.name=='nt' else 'clear')

4e02a81096e998bef538702d8caeb249aa1c72eb
Beardinidas <33542863+Beardinidas@users.noreply.github.com>
2022-03-27 13:44:21 +0100

    Encode Data Added

--- QRCode/main.py
+++ QRCode/main.py
@@ -2,6 +2,8 @@
 
 import random
 import os
+import qrcode
+from datetime import datetime
 
 class Style:
 	RESET = '\033[0m'
@@ -13,6 +15,52 @@ class Style:
 
 
 def main():
+	ed = ''
+	while ed != 'E' or ed != 'D':
+		clearConsole()
+		print(f'E: Encode')
+		print(f'D: Decode')
+		ed = input('Selection: ').upper()
+
+		if ed == 'E':
+			qrEncode()
+			print('Encoding Data Complete!')
+			break
+		if ed == 'D':
+			qrDecode()
+
+
+
+def qrEncode():
+	print('ENCODE')
+
+	end = ''
+	counter = 0
+	date = datetime.today().strftime('%Y%m%d')
+	print(date)
+	while end != 'exit!':
+		clearConsole()
+		data = input('What do you want to Encode into a QR Code?: ')
+		#Data to Encode into a QR Code Saved into qrCodeImg
+		qrCodeImg = qrcode.make(data)
+		type(qrCodeImg)
+		#Save the QRCode into the encode Folder
+		qrCodeImg.save(f'./qrs/encode/qrCode_{date}_{counter}.png')
+
+		#Write QRCode Image file name and Data Encoded to txt file in Encode Folder
+		with open('./qrs/encode/qrCodeData.txt', 'a') as f:
+			f.write(f'qrCode_{date}_{counter}: {data}\n')
+
+		clearConsole()
+		end = input(f'To Encode More Data Hit Enter else type {Style.RED}exit!{Style.RESET}: ').lower()
+		counter += 1
+
+
+def qrDecode():
+	print('DECODE')
+	print(f'{Style.RED}Please Ensure your QR Data Files are In the Correct Folder!{Style.RESET}')
+	input('Press Enter to Continue')
+
 	print('TODO')
 
 

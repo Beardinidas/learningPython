af62fb4dc2c4340f773e3df9e425d9e5b3e7edfd
Beardinidas <33542863+Beardinidas@users.noreply.github.com>
2022-03-27 15:22:47 +0100

    Updated Encoding Naming Format

--- QRCode/main.py
+++ QRCode/main.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+##!/usr/bin/python
 
 import random
 import os
 import qrcode
-from datetime import datetime
+import time
+
 
 class Style:
 	RESET = '\033[0m'
@@ -28,6 +29,8 @@ def main():
 			break
 		if ed == 'D':
 			qrDecode()
+			print('Decoding QRCodes Complete!')
+			break
 
 
 
@@ -35,30 +38,31 @@ def qrEncode():
 	print('ENCODE')
 
 	end = ''
-	counter = 0
-	date = datetime.today().strftime('%Y%m%d')
-	print(date)
 	while end != 'exit!':
 		clearConsole()
+		#Ensures Unique File Name
+		uts = int(time.time())
 		data = input('What do you want to Encode into a QR Code?: ')
 		#Data to Encode into a QR Code Saved into qrCodeImg
 		qrCodeImg = qrcode.make(data)
 		type(qrCodeImg)
 		#Save the QRCode into the encode Folder
-		qrCodeImg.save(f'./qrs/encode/qrCode_{date}_{counter}.png')
+		qrCodeImg.save(f'./qrs/encode/qrCode_{uts}.png')
 
 		#Write QRCode Image file name and Data Encoded to txt file in Encode Folder
+		#This will Create the qrCodeData File if it Does Not Exist and Append Data to the end
 		with open('./qrs/encode/qrCodeData.txt', 'a') as f:
-			f.write(f'qrCode_{date}_{counter}: {data}\n')
+			f.write(f'qrCode_{uts}: {data}\n')
 
 		clearConsole()
 		end = input(f'To Encode More Data Hit Enter else type {Style.RED}exit!{Style.RESET}: ').lower()
-		counter += 1
 
 
 def qrDecode():
+	clearConsole()
 	print('DECODE')
 	print(f'{Style.RED}Please Ensure your QR Data Files are In the Correct Folder!{Style.RESET}')
+	print(f'Note only {Style.GREEN}.png{Style.RESET} are supported')
 	input('Press Enter to Continue')
 
 	print('TODO')

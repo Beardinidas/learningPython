40168141bcf6aad1ed8af1f1d354eb5f79d5b775
Beardinidas <33542863+Beardinidas@users.noreply.github.com>
2022-03-29 14:49:08 +0100

    Bulk File Renamer Complete

--- BulkFileRenamer/README.md
+++ BulkFileRenamer/README.md
@@ -1,3 +1,7 @@
 # Bulk File Renamer
 
 
+Bulk File Rename Tool  
+Note: This is used for my Astrophotography but can be used to rename anyother files  
+
+
--- BulkFileRenamer/main.py
+++ BulkFileRenamer/main.py
@@ -2,6 +2,7 @@
 
 import os
 
+
 class Style:
 	RESET = '\033[0m'
 	RED = '\033[31m'
@@ -11,11 +12,40 @@ class Style:
 	underLine = '\033[4m'
 
 
+files = './files/'
 
 def main():
-	print('TODO')
+	print(f'{Style.GREEN}BULK FILE RENAMER{Style.RESET}\n')
+	print(f'{Style.RED}Please Ensure Files are in the \'files\' Folder{Style.RESET}')
+	input('Press Enter to Continue')
+
+	clearConsole()
+
+	capture = input(f'What has been captured?: ').strip()
+
+	fileCounter = 0
+
+	#Change Directory to Files Folder
+	os.chdir(files)
+
+	print('Renaming Files...')
+	for file in os.listdir():
+		fileName, fileExt = os.path.splitext(file)
+
+		#Reassign fileName to Capture
+		#zfill will pad counter to 3 digits
+		fileName = f'{capture}_{str(fileCounter).zfill(3)}'
+
+		#New FileName with Original Extension
+		newFileName = f'{fileName}{fileExt}'
+
+		#Rename File
+		os.rename(file, newFileName)
+
+		fileCounter += 1
 
 
+	print(f'{Style.GREEN}Renaming Complete!{Style.RESET}')
 
 def clearConsole():
         os.system('cls' if os.name=='nt' else 'clear')
